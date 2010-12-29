ea439446fecb12a3c55e9799b2f87e8058f5fa18
Mathias Lafeldt <misfire@debugon.org>
2010-12-29 10:32:53 +0100

    Check if cb2util has been built before testing

---
+++ t/t0000-basic.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+test_description="Test very basics"
+
+../cb2util >/dev/null
+if [ $? != 1 ]; then
+    echo >&2 'You do not seem to have built cb2util yet.'
+    exit 1
+fi
+
+. ./test-lib.sh
+
+test_expect_success "success is reported like this" "
+    :
+"
+
+test_done
