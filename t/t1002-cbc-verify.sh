bcbbff94074c0b3431b6e37594163b79a0cf08ff
Mathias Lafeldt <misfire@debugon.org>
2010-12-31 13:22:52 +0100

    Allow to test for invalid signatures

--- t/t1002-cbc-verify.sh
+++ t/t1002-cbc-verify.sh
@@ -5,19 +5,28 @@ test_description="cb2util cbc: verify signature"
 . ./test-lib.sh
 
 for file in $TEST_DIR/cbc/*.cbc; do
+    prefix=${file%.*}
     basename=$(basename "$file")
 
-    test_expect_success "$basename" "
-        cb2util cbc -c $file >out &&
-        echo '$file: OK' >expect &&
-        test_cmp expect out
-    "
+    if [ -f "$prefix.ok" ]; then
+        test_expect_success "$basename valid" "
+            cb2util cbc -c $file >out &&
+            echo '$file: OK' >expect &&
+            test_cmp expect out
+        "
+    else
+        test_expect_success "$basename invalid" "
+            test_must_fail cb2util cbc -c $file >out &&
+            echo '$file: FAILED' >expect &&
+            test_cmp expect out
+        "
+    fi
 done
 
 for file in $TEST_DIR/cbc7/*.cbc; do
     basename=$(basename "$file")
 
-    test_expect_success "$basename (v7 has no signature)" "
+    test_expect_success "$basename (v7) not signed" "
         test_must_fail cb2util cbc -7 -c $file
     "
 done
--- t/t3003-pcb-verify.sh
+++ t/t3003-pcb-verify.sh
@@ -5,13 +5,22 @@ test_description="cb2util pcb: verify signature"
 . ./test-lib.sh
 
 for file in $TEST_DIR/pcb/*.bin; do
+    prefix=${file%.*}
     basename=$(basename "$file")
 
-    test_expect_success "$basename" "
-        cb2util pcb -c $file >out &&
-        echo '$file: OK' >expect &&
-        test_cmp expect out
-    "
+    if [ -f "$prefix.ok" ]; then
+        test_expect_success "$basename valid" "
+            cb2util pcb -c $file >out &&
+            echo '$file: OK' >expect &&
+            test_cmp expect out
+        "
+    else
+        test_expect_success "$basename invalid" "
+            test_must_fail cb2util pcb -c $file >out &&
+            echo '$file: FAILED' >expect &&
+            test_cmp expect out
+        "
+    fi
 done
 
 test_done
