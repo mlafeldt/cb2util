ed952ee5c634d38beecd15a55785323318988b54
Mathias Lafeldt <misfire@debugon.org>
2011-03-06 21:02:57 +0100

    Add missing license notice to compress.[ch]

--- compress.c
+++ compress.c
@@ -1,3 +1,26 @@
+/*
+ * zlib wrapper
+ *
+ * Copyright (C) 2006-2011 Mathias Lafeldt <misfire@debugon.org>
+ * All rights reserved.
+ *
+ * This file is part of cb2util, the CodeBreaker PS2 File Utility.
+ *
+ * cb2util is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * cb2util is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 #include <stdlib.h>
 #include <zlib.h>
 
