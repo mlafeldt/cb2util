9477f94e1900b851e43ec34705693646ac053a50
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2012-07-27 13:22:25 +0200

    Replace GPL header with MIT header in source files

--- sha1.h
+++ sha1.h
@@ -2,23 +2,10 @@
  * Wrapper for SHA-1 implementation
  *
  * Copyright (C) 2006-2012 Mathias Lafeldt <mathias.lafeldt@gmail.com>
- * All rights reserved.
  *
  * This file is part of cb2util, the CodeBreaker PS2 File Utility.
  *
- * cb2util is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * cb2util is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * cb2util is licensed under the terms of the MIT License. See LICENSE file.
  */
 
 #ifndef _SHA1_H_
