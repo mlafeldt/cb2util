67cf2a9f782b7c31a70bd43a4e6d3cde8b92512f
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2017-06-07 13:32:29 +0200

    rustfmt rfc

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,16 @@ fn main() {
     let m = App::new("cb2util")
         .version(crate_version!())
         .setting(AppSettings::SubcommandRequired)
-        .subcommand(SubCommand::with_name("cheats")
-            .about("Read or write CodeBreaker \"cheats\" files")
-            .args(&[Arg::from_usage("-d, --decrypt 'Decrypt extracted cheats'"),
-                    Arg::from_usage("<INPUT> 'Path to \"cheats\" file to read'")]))
+        .subcommand(
+            SubCommand::with_name("cheats")
+                .about("Read or write CodeBreaker \"cheats\" files")
+                .args(
+                    &[
+                        Arg::from_usage("-d, --decrypt 'Decrypt extracted cheats'"),
+                        Arg::from_usage("<INPUT> 'Path to \"cheats\" file to read'"),
+                    ],
+                ),
+        )
         .get_matches();
 
     match m.subcommand() {
