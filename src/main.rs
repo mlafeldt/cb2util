73a792378527fec1b47c2a539c2910359e56124e
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2020-04-30 22:57:56 +0200

    Run crypto tests

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ use clap::{App, AppSettings, Arg, SubCommand};
 mod cli;
 use cli::cheats;
 
+#[allow(unused_imports)]
+#[allow(dead_code)]
+mod crypto;
+
 fn main() {
     let m = App::new("cb2util")
         .version(crate_version!())
