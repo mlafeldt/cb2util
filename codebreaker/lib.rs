48e5f4926bdd35c61bd6fbbaf845f4d442f0e83b
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2020-05-01 12:06:34 +0200

    Expose CB file functions

--- codebreaker/lib.rs
+++ codebreaker/lib.rs
@@ -2,13 +2,20 @@ pub mod cb1;
 pub mod cb7;
 
 extern crate libc;
+use libc::{c_int, size_t};
 
+// TODO: port all these functions to pure Rust
 extern "C" {
+    // All versions
     pub fn cb_reset();
     pub fn cb_set_common_v7();
     pub fn cb_encrypt_code(addr: *mut u32, val: *mut u32);
     pub fn cb_decrypt_code(addr: *mut u32, val: *mut u32);
     pub fn cb_decrypt_code2(addr: *mut u32, val: *mut u32);
+
+    // CB file functions
+    pub fn cb_verify_signature(sig: *const u8, buf: *const u8, buflen: size_t) -> c_int;
+    pub fn cb_crypt_data(buf: *mut u8, buflen: size_t);
 }
 
 pub fn reset() {
@@ -29,4 +36,12 @@ pub fn decrypt_code(addr: &mut u32, val: &mut u32) {
 
 pub fn decrypt_code2(addr: &mut u32, val: &mut u32) {
     unsafe { cb_decrypt_code2(addr, val) }
+}
+
+pub fn verify_signature(sig: &[u8], buf: &[u8]) -> bool {
+    unsafe { cb_verify_signature(sig.as_ptr(), buf.as_ptr(), buf.len() as size_t) == 0 }
+}
+
+pub fn crypt_data(buf: &mut [u8]) {
+    unsafe { cb_crypt_data(buf.as_mut_ptr(), buf.len() as size_t) }
 }
